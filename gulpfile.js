ff532a4ffd77f5d3e3326a4beb30822c4feb4d24
Cameron Messinides <cameron.messinides@gmail.com>
2017-01-01 16:04:41 -0500

    Migrate hugo files to a theme

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,16 @@ const gulp = require('gulp'),
       autoprefix = require('gulp-autoprefixer');
 
 gulp.task('sass', function() {
-  return gulp.src('./sass/**/*.scss')
+  return gulp.src('./themes/colmag/sass/**/*.scss')
     .pipe(sass({
       outputStyle: 'compressed'
     }).on('error', sass.logError))
     .pipe(autoprefix({
       browsers: ['last 2 versions', '> 5% in US']
     }))
-    .pipe(gulp.dest('./static/css'));
+    .pipe(gulp.dest('./themes/colmag/static/css'));
 });
 
 gulp.task('watch', ['sass'], function() {
-  gulp.watch('./sass/**/*.scss', ['sass'])
+  gulp.watch('./themes/colmag/sass/**/*.scss', ['sass'])
 })
