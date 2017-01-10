be0ee70ef04ac5f57a378fd558a75b0c01bc5321
Cameron Messinides <cameron.messinides@gmail.com>
2017-01-09 20:54:48 -0500

    Create a image resizing process

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,64 @@
 'use strict';
 
-const gulp = require('gulp'),
-      sass = require('gulp-sass'),
-      autoprefix = require('gulp-autoprefixer');
+const gulp = require('gulp');
+// pre-processing stylesheets
+const sass = require('gulp-sass');
+const autoprefix = require('gulp-autoprefixer');
+// resizing images
+const imageResize = require('gulp-image-resize');
+const changed = require('gulp-changed');
 
+// compiling, autoprefixing, and minifying sass
 gulp.task('sass', function() {
-  return gulp.src('./assets/sass/**/*.scss')
+  return gulp.src('assets/sass/**/*.scss')
     .pipe(sass({
       outputStyle: 'compressed'
     }).on('error', sass.logError))
     .pipe(autoprefix({
       browsers: ['last 2 versions', '> 5% in US']
     }))
-    .pipe(gulp.dest('./static/assets/css'));
+    .pipe(gulp.dest('static/assets/css'));
 });
 
+// Resizing images
+
+const imageSrc = 'assets/img/**/*.{jpg,jpeg,png,tiff}';
+var imageResizeTasks = [];
+
+[100,300,500,800,1000].forEach(function(size) {
+  let imageResizeTask = 'resize_' + size;
+  gulp.task(imageResizeTask, function() {
+    let dest = `static/assets/img/${size}`
+    return gulp.src(imageSrc)
+      .pipe(changed(dest, {extension: '.jpg'}))
+      .pipe(imageResize({
+        filter: 'Catrom',
+        format: 'jpg',
+        noProfile: true,
+        quality: 0.9,
+        width: size
+      }))
+      .pipe(gulp.dest(dest));
+  });
+  imageResizeTasks.push(imageResizeTask);
+});
+
+// Transfering and compressing original images
+gulp.task('original_images', function() {
+  let dest = 'static/assets/img'
+  return gulp.src(imageSrc)
+    .pipe(changed(dest, {extension: '.jpg'}))
+    .pipe(imageResize({
+      format: 'jpg',
+      noProfile: true,
+      quality: 0.9
+    }))
+    .pipe(gulp.dest(dest));
+});
+imageResizeTasks.push('original_images');
+
+gulp.task('images', imageResizeTasks);
+
 gulp.task('watch', ['sass'], function() {
-  gulp.watch('./assets/sass/**/*.scss', ['sass'])
-})
+  gulp.watch('assets/sass/**/*.scss', ['sass'])
+});
