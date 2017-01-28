9b78cc11dadc8c3fd03c85cad80bed8fb5f59ee0
Cameron Messinides <cameron.messinides@gmail.com>
2017-01-27 23:05:33 -0500

    Make the menu keyboard accessible

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,9 +1,25 @@
+var menu = document.getElementById('header-menu');
+var toggle = document.getElementById('menu-toggle');
+var menuLinks = document.getElementsByClassName('header__nav-link');
+
 var toggleMenu = function() {
-  var menu = document.getElementById('header-menu');
   menu.classList.toggle('active');
+  toggle.classList.toggle('active');
 };
 
-var toggles = document.getElementsByClassName('js-menu-toggle');
-[].forEach.call(toggles, function(toggle) {
-  toggle.addEventListener('click', toggleMenu);
+var toggleMenuOnFocus = function() {
+  if (document.activeElement.classList.contains('header__nav-link')) {
+    menu.classList.add('active');
+    toggle.classList.add('active');
+  } else {
+    menu.classList.remove('active');
+    toggle.classList.remove('active');
+  }
+};
+
+toggle.addEventListener('click', toggleMenu);
+
+[].forEach.call(menuLinks, function(menuLink) {
+  menuLink.addEventListener('focus', toggleMenuOnFocus);
+  menuLink.addEventListener('blur', toggleMenuOnFocus);
 });
