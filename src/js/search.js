3e0e55c7189350855e9d6cdbe47058780c0a9806
Cameron Messinides <cameron.messinides@gmail.com>
2017-02-03 00:15:40 -0500

    WIP: Search can now load and index data;

--- src/js/search.js
+++ src/js/search.js
@@ -1,148 +1,4 @@
-/* create the search index as a singleton */
-var ColMagSearchIndex;
-(function(){
-  var instance;
-
-  ColMagSearchIndex = function ColMagSearchIndex() {
-    if (instance) {
-      return instance;
-    };
-
-    instance = this;
-
-    /* Index properties and functions go here */
-    var types = ['announcements', 'articles', 'pages', 'staff'];
-    this.models = {};
-    var models = this.models;
-    this.indices = {
-      'announcements': lunr(function() {
-        this.field('title', {boost: 20});
-        this.field('description');
-        this.field('content');
-        this.field('tags', {boost: 30});
-        this.ref('id');
-      }),
-      'articles': lunr(function() {
-        this.field('title', {boost: 20});
-        this.field('description');
-        this.field('content');
-        this.field('tags', {boost: 30});
-        this.field('categories', {boost: 20});
-        this.field('authors');
-        this.ref('id');
-      }),
-      'pages': lunr(function() {
-        this.field('title', {boost: 30});
-        this.field('description');
-        this.field('content');
-        this.ref('id');
-      }),
-      'staff': lunr(function() {
-        this.field('title', {boost: 30});
-        this.field('roles', {boost: 10});
-        this.field('bio');
-        this.ref('id');
-      })
-    };
-    var indices = this.indices;
-
-    this.getModel = function(type) {
-      var req = new XMLHttpRequest();
-      var uri = '/search/' + type + '.json';
-      var modelData;
-      req.open('GET', uri, true);
-
-      req.onload = function() {
-        if (this.status >= 200 && this.status < 400) {
-          modelData = JSON.parse(this.response);
-          models[type] = modelData;
-        } else {
-          console.log('ERROR: Received failing status code ' + this.status + ' for GET ' + uri);
-        }
-      }
-
-      req.onerror = function() {
-        console.log('ERROR: Failed to load ' + uri);
-      }
-
-      req.send();
-    }
-
-    types.forEach(function(type) { this.getModel(type) });
-
-    this.build = function() {
-      if (models['announcements']) {
-        models['announcements'].forEach(function(m) {
-          indices['announcements'].add({
-            id: m['id'],
-            title: m['title'],
-            description: m['description'],
-            content: m['content'],
-            tags: m['tags'].join(' ')
-          });
-        })
-      }
-
-      if (models['articles']) {
-        models['articles'].forEach(function(m) {
-          indices['articles'].add({
-            id: m['id'],
-            title: m['title'],
-            description: m['description'],
-            content: m['content'],
-            tags: m['tags'].join(' '),
-            categories: m['categories'].join(' '),
-            authors: m['authors'].join(' ')
-          });
-        });
-      }
-
-      if (models['pages']) {
-        models['pages'].forEach(function(m) {
-          indices['pages'].add({
-            id: m['id'],
-            title: m['title'],
-            description: m['description'],
-            content: m['content']
-          });
-        });
-      }
-
-      if (models['staff']) {
-        models['staff'].forEach(function(m) {
-          indices['staff'].add({
-            id: m['id'],
-            title: m['title'],
-            roles: m['roles'].join(' '),
-            bio: m['bio']
-          });
-        });
-      }
-    };
-
-    this.search = function(query, types) {
-      var resultsMap = {};
-      var results;
-      types.forEach(function(type) {
-        results = indices[type].search(query);
-        if (results.length) {
-          resultsMap[type] = results;
-        }
-      });
-      return resultsMap;
-    };
-
-    this.announcements = indices['announcements'];
-    this.articles = indices['articles'];
-    this.pages = indices['pages'];
-    this.staff = indices['staff'];
-
-
-    return instance;
-  }
-}());
-
-// And a singleton search agent
+// Contain everything in a singleton search agent
 var ColMagSearchAgent;
 (function(){
   var instance;
@@ -154,9 +10,131 @@ var ColMagSearchAgent;
 
     instance = this;
 
-    this.registerIndex = function(index) {
-      this.index = index;
-    };
+    var contentTypes = [
+      {
+        name: 'announcements',
+        indexCallback: function() {
+          this.field('title', {boost: 20});
+          this.field('description');
+          this.field('content');
+          this.field('tags', {boost: 30});
+          this.ref('id');
+        }
+      },
+      {
+        name: 'articles',
+        indexCallback: function() {
+          this.field('title', {boost: 20});
+          this.field('description');
+          this.field('content');
+          this.field('tags', {boost: 30});
+          this.field('categories', {boost: 20});
+          this.field('authors');
+          this.ref('id');
+        }
+      },
+      {
+        name: 'pages',
+        indexCallback: function() {
+          this.field('title', {boost: 20});
+          this.field('description');
+          this.field('content');
+          this.ref('id');
+        }
+      },
+      {
+        name: 'staff',
+        indexCallback: function() {
+          this.field('title', {boost: 20});
+          this.field('roles', {boost: 10});
+          this.field('bio');
+          this.ref('id');
+        }
+      }
+    ];
+
+    this.dataLists = {};
+    this.indices = {};
+    this.templates = {};
+
+    this.getResource = function(uri, type, success, error) {
+      var req = new XMLHttpRequest();
+      req.open('GET', uri, true);
+
+      req.onload = function() {
+        if (this.status >= 200 && this.status < 400) {
+          success(this.response, type);
+        } else {
+          error();
+        }
+      }
+
+      req.onerror = function() {
+        error();
+      }
+
+      req.send();
+    }
+
+    this.buildIndices = function() {
+      contentTypes.forEach(function(type) {
+        this.indices[type.name] = lunr(type.indexCallback);
+      })
+    }
+
+    this.loadDataLists = function() {
+      contentTypes.forEach(function(type) {
+        var uri = '/search/data/' + type.name + '.json';
+        this.getResource(uri, type, this.dataLoaded, function(){});
+      })
+    }
+
+    this.dataLoaded = function(response, type) {
+      this.storeDataList(response, type);
+      this.fillIndex(type);
+    }
+
+    this.storeDataList = function(response, type) {
+      this.dataLists[type.name] = JSON.parse(response);
+    }
+
+    this.fillIndex = function(type) {
+      var dataList = this.dataLists[type.name];
+      var index = this.indices[type.name];
+      var fields = index._fields.map(function(f){ return f.name });
+      var ref = index._ref;
+      var entry;
+
+      dataList.forEach(function(dataItem) {
+        entry = {};
+        fields.forEach(function(field) {
+          if (dataItem[field] instanceof Array) {
+            entry[field] = dataItem[field].join(' ');
+          } else {
+            entry[field] = dataItem[field] || "";
+          }
+        });
+        entry[ref] = dataItem[ref];
+        index.add(entry);
+      })
+    }
+
+    this.loadTemplates = function() {
+      contentTypes.forEach(function(type) {
+        var uri = '/search/views/' + type.name + '.hbs';
+        this.getResource(uri, type, this.storeTemplate, function(){});
+      })
+    }
+
+    this.storeTemplate = function(response, type) {
+      this.templates[type.name] = response;
+    }
+
+    this.build = function() {
+      searchAgent.buildIndices();
+      searchAgent.loadDataLists();
+      searchAgent.loadTemplates();
+    }
 
     // TODO: Add render methods
     // TODO: Add url getting and setting methods
@@ -166,4 +144,4 @@ var ColMagSearchAgent;
 }());
 
 var searchAgent = ColMagSearchAgent();
-searchAgent.registerIndex(ColMagSearchIndex());
+searchAgent.build();
