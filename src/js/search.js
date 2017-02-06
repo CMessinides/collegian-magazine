f7cac18186f2a5f555de7b9718c1d825d0a59d8c
Cameron Messinides <cameron.messinides@gmail.com>
2017-02-06 06:50:47 -0500

    WIP: Search. Models functional. Working on router.

--- src/js/search.js
+++ src/js/search.js
@@ -1,282 +1,357 @@
-// Contain everything in a singleton search agent
-var ColMagSearchAgent;
 (function(){
-  var instance;
 
-  ColMagSearchAgent = function ColMagSearchAgent() {
-    if (instance) {
-      return instance;
-    };
+  function SearchError(msg, callbacks) {
+    this.msg = msg || "An error occurred in the search agent. Search agent is shutting down...";
+    this.callbacks = callbacks || [];
+  }
 
-    instance = this;
+  function SearchDependencyError(msg, callbacks) {
 
-    function BuildError(message, callbacks) {
-      this.message = message || "An error occurred during the search agent's build process. Search agent is shutting down...";
-      this.callbacks = callbacks || [];
+  }
+
+  function SearchValidationError(msg, callbacks) {
+
+  }
+
+  function SearchBuildError(msg, callbacks) {
+    var base = SearchError;
+    base(msg, callbacks);
+  }
+
+  SearchBuildError.prototype = Object.create(SearchError.prototype);
+
+  function SearchAgent() {
+    // ## Error handling
+    this.shutdown = function(e) {
+      throw e
     }
 
-    this.config = {
-      elementIds: {
-        searchForm: 'search',
-        searchField: 'search-field',
-        searchSubmit: 'search-submit',
-        searchFilters: 'search-filters',
-        searchResults: 'search-results',
+    try {
+      if (!window.Handlebars) {
+        throw new SearchDependencyError('Handlebars');
+      }
+      if (!window.lunr) {
+        throw new SearchDependencyError('lunr');
+      }
+    } catch (e) {
+      this.shutdown(e);
+    }
+
+    var config = {
+      elements: {
+        form: 'search',
+        field: 'search-field',
+        submit: 'search-submit',
+        filters: 'search-filters',
+        results: 'search-results',
+        loadingMsg: 'search-loading',
         errorMsg: 'search-error'
       },
       dataMethod: 'xhr',
       templateMethod: 'xhr',
       xhr: {
-        protocol: window.location.protocol,
+        protocol: window.location.protocol + '//',
         hostname: window.location.hostname,
-        basePath: '/',
-        dataPath: '',
-        templatePath: ''
+        port: window.location.port ? ":" + window.location.port : "",
+        basePath: '/search/',
+        dataPath: 'data/',
+        templatePath: 'views/'
       }
     };
 
-    this.getElements = function() {
-      Object.keys(this.config.elementIds).forEach(function(key) {
-        try {
+    Object.freeze(config);
 
-        } catch (e) {
-
-        } finally {
-
-        }
-      })
-    }
-
-    this.models = {};
-
-    this.insertModels = function(models) {
-      Object.keys(models).forEach(this.insertModel(name, models[name]));
-    }
-
-    this.insertModel = function(name, model) {
-      var requiredProps = {
-        indexCallback: 'function'
-      };
-      Object.keys(requiredProps).forEach(function(prop) {
-        if (typeof model[prop] === requiredProps[prop]) {
-          return;
-        } else {
-          throw new BuildError();
-        }
-      })
-      this.models[name] = model;
-    }
-
-    var contentTypes = [
-      {
-        name: 'announcements',
-        indexCallback: function() {
-          this.field('title', {boost: 20});
-          this.field('description');
-          this.field('content');
-          this.field('tags', {boost: 30});
-          this.ref('id');
-        }
-      },
-      {
-        name: 'articles',
-        indexCallback: function() {
-          this.field('title', {boost: 20});
-          this.field('description');
-          this.field('content');
-          this.field('tags', {boost: 30});
-          this.field('categories', {boost: 20});
-          this.field('authors');
-          this.ref('id');
-        }
-      },
-      {
-        name: 'pages',
-        indexCallback: function() {
-          this.field('title', {boost: 20});
-          this.field('description');
-          this.field('content');
-          this.ref('id');
-        }
-      },
-      {
-        name: 'staff',
-        indexCallback: function() {
-          this.field('title', {boost: 20});
-          this.field('roles', {boost: 10});
-          this.field('bio');
-          this.ref('id');
+    // utility functions
+    function joinObj(obj) {
+      var array = [];
+      for (var p in obj) {
+        if (obj.hasOwnProperty(p)) {
+          array.push(obj[p]);
         }
       }
-    ];
+      return array.join(' ');
+    }
 
-    this.dataLists = {};
-    this.indices = {};
-    this.templates = {};
-    this.currentParams;
-    this.currentQuery;
-    this.currentExclude;
-
-    this.searchBar = document.getElementById('main-search');
-
-    this.getResource = function(uri, type, success, error) {
+    function getResource(path, target, callbacks) {
       var req = new XMLHttpRequest();
+      var uri = config.xhr.protocol +
+                config.xhr.hostname +
+                config.xhr.port +
+                config.xhr.basePath +
+                path;
       req.open('GET', uri, true);
 
       req.onload = function() {
         if (this.status >= 200 && this.status < 400) {
-          success(this.response, type);
+          for (var i in callbacks) {
+            callbacks[i].call(target, this.response)
+          }
         } else {
-          error();
+          throw new SearchBuildError();
         }
       }
 
       req.onerror = function() {
-        error();
+        throw new SearchBuildError();
       }
 
       req.send();
     }
 
-    this.buildIndices = function() {
-      contentTypes.forEach(function(type) {
-        this.indices[type.name] = lunr(type.indexCallback);
+    // ## Models
+
+    // These are Model-specific methods; they should not be called on any objects that are not instances of Model
+    function validateModel(initObj) {
+      var requiredProps = {
+        name: 'string',
+        indexCallback: 'function'
+      };
+      for (var prop in requiredProps) {
+        if (typeof initObj[prop] === requiredProps[prop]) {
+          return;
+        } else {
+          throw new SearchValidationError();
+        }
+      }
+    }
+
+    function buildIndex() {
+      Object.defineProperty(this, 'index', {
+        value: lunr(this.indexCallback)
       })
     }
 
-    this.loadDataLists = function() {
-      contentTypes.forEach(function(type) {
-        var uri = '/search/data/' + type.name + '.json';
-        this.getResource(uri, type, this.dataLoaded, function(){});
+    function storeData(json) {
+      try {
+        Object.defineProperty(this, 'data', {
+          value: JSON.parse(json)
+        });
+      } catch (e) {
+        if (e instanceof SyntaxError) {
+          throw new SearchBuildError()
+        } else {
+          throw e
+        }
+      }
+    }
+
+    function storeTemplate(hbs) {
+      Object.defineProperty(this, 'render', {
+        value: Handlebars.compile(hbs)
       })
     }
 
-    this.dataLoaded = function(response, type) {
-      this.storeDataList(response, type);
-      this.fillIndex(type);
-    }
-
-    this.storeDataList = function(response, type) {
-      this.dataLists[type.name] = JSON.parse(response);
-    }
-
-    this.fillIndex = function(type) {
-      var dataList = this.dataLists[type.name];
-      var index = this.indices[type.name];
-      var fields = index._fields.map(function(f){ return f.name });
-      var ref = index._ref;
+    function fillIndex(ignore) {
+      ignore = null;
+      var fields = this.index._fields.map(function(f){ return f.name });
+      var ref = this.index._ref;
       var entry;
 
-      dataList.forEach(function(dataItem) {
+      for (var index in this.data) {
+        var item = this.data[index];
         entry = {};
-        fields.forEach(function(field) {
-          if (dataItem[field] instanceof Array) {
-            entry[field] = dataItem[field].join(' ');
+        for (var f in fields) {
+          var field = fields[f];
+          if (item[field] instanceof Array) {
+            entry[field] = item[field].join(' ');
           } else {
-            entry[field] = dataItem[field] || "";
+            entry[field] = item[field] || "";
           }
-        });
-        entry[ref] = dataItem[ref];
-        index.add(entry);
-      })
-    }
-
-    this.loadTemplates = function() {
-      contentTypes.forEach(function(type) {
-        var uri = '/search/views/' + type.name + '.hbs';
-        this.getResource(uri, type, this.storeTemplate, function(){});
-      })
-    }
-
-    this.storeTemplate = function(response, type) {
-      this.templates[type.name] = response;
-    }
-
-    this.initializeFromURLParams = function(callbacks) {
-      var search = window.location.search;
-      var params = {};
-      if (search) {
-        search.slice(1).split('&').forEach(function(param) {
-          var pair = param.split('=');
-          var decoded = decodeURIComponent(pair[1]).split('+').join(' ').trim();
-          params[pair[0]] = decoded.search(',') > -1 ? decoded.split(',') : decoded;
-        })
-      }
-      console.log(params);
-      this.currentParams = params;
-      this.currentQuery = params.q;
-      this.currentExclude = params.exclude;
-
-      if (callbacks instanceof Array) {
-        callbacks.forEach(function(callback) {
-          if (typeof callback === 'function') { callback(params) };
-        })
+        }
+        entry[ref] = parseInt(index);
+        this.index.add(entry);
       }
     }
 
-    this.setFormValues = function(params) {
-      this.searchField.value = this.currentQuery || "";
+    // Model constructors
+    function Model(initObj) {
+      Object.defineProperty(this, 'validate', {
+        value: validateModel
+      });
+
+      this.validate(initObj);
+
+      for (var key in initObj) {
+        if (initObj.hasOwnProperty(key)) {
+          Object.defineProperty(this, key, {
+            value: initObj[key]
+          })
+        }
+      }
+
+      Object.defineProperties(this, {
+        buildIndex: {
+          value: buildIndex
+        },
+        getData: {
+          value: function() {
+            var path = config.xhr.dataPath +
+                        encodeURIComponent(this.name) +
+                        '.json';
+            getResource(path, this, [storeData, fillIndex]);
+          }
+        },
+        storeData: {
+          value: storeData
+        },
+        fillIndex: {
+          value: fillIndex
+        },
+        getTemplate: {
+          value: function() {
+            var path = config.xhr.templatePath +
+                       encodeURIComponent(this.name) +
+                       '.hbs';
+            getResource(path, this, [storeTemplate]);
+          }
+        },
+        storeTemplate: {
+          value: storeTemplate
+        }
+      });
     }
 
-    var initializeHistory = function(params) {
-      window.history.state = this.currentParams;
+    this.models = {};
+
+    // ## Router
+    // Conveys state to the search agent
+    // Directs queries from the search form to the models, sets and gets the history state, and renders the search results in the document
+    this.router = {
+      listeners: {}
+    };
+
+    function storeElements() {
+      var optional = ['filters', 'loadingMsg'];
+      Object.defineProperty(this, 'elements', {
+        value: {}
+      })
+
+      for (var key in config.elements) {
+        var el = document.getElementById(config.elements[key]);
+        if (!el && !optional.includes(key)) {
+          throw new SearchBuildError();
+        }
+        Object.defineProperty(this.elements, key, {
+          enumerable: true,
+          value: el
+        })
+      }
+
+      if (this.elements.filters) {
+        this.storeFilters();
+      }
     }
 
-    this.build = function() {
-      this.buildIndices();
-      this.loadDataLists();
-      this.loadTemplates();
-      this.initializeFromURLParams([initializeHistory, this.setFormValues]);
+    function storeFilters() {
+
     }
 
-    // TODO: Add render methods
-    // TODO: Add url getting and setting methods
+    function initializeState() {
 
-    return instance;
+    }
+
+    function buildSearchSchema() {
+
+    }
+
+    Object.defineProperties(this.router, {
+      initialize: {
+        value: function() {
+          this.storeElements();
+          this.buildSearchSchema();
+          this.initializeState();
+        }
+      }
+    })
+
+    // Top-level and alias functions
+    this.insert = function(models) {
+      for (var key in models) {
+        if (models.hasOwnProperty(key)) {
+          var model = models[key];
+          model.name = key;
+          Object.defineProperty(this.models, key, {
+            enumerable: true,
+            value: new Model(model)
+          })
+        }
+      }
+    }
+
+    this.start = function() {
+      try {
+        this.router.initialize()
+
+        for (var m in this.models) {
+          this.models[m].buildIndex();
+          this.models[m].getData();
+          this.models[m].getTemplate();
+        }
+      } catch (e) {
+        this.shutdown(e);
+      }
+    }
+
+  }
+
+
+  if (typeof window.SearchAgent === 'undefined') {
+    window.SearchAgent = SearchAgent;
+  } else {
+    throw "SearchAgent is already defined in the global scope."
   }
 }());
 
-var searchModels = {
-  announcements: {
-    indexCallback: function() {
-      this.field('title', {boost: 20});
-      this.field('description');
-      this.field('content');
-      this.field('tags', {boost: 30});
-      this.ref('id');
-    }
-  },
+// activating the search agent
+// (function(){
+  var searchModels = {
+    announcements: {
+      indexCallback: function() {
+        this.field('title', {boost: 20});
+        this.field('description');
+        this.field('content');
+        this.field('tags', {boost: 30});
+        this.ref('id');
+      }
+    },
 
-  articles: {
-    indexCallback: function() {
-      this.field('title', {boost: 20});
-      this.field('description');
-      this.field('content');
-      this.field('tags', {boost: 30});
-      this.field('categories', {boost: 20});
-      this.field('authors');
-      this.ref('id');
-    }
-  },
+    articles: {
+      indexCallback: function() {
+        this.field('title', {boost: 20});
+        this.field('description');
+        this.field('content');
+        this.field('tags', {boost: 30});
+        this.field('categories', {boost: 20});
+        this.field('authors');
+        this.ref('id');
+      }
+    },
 
-  pages: {
-    indexCallback: function() {
-      this.field('title', {boost: 20});
-      this.field('description');
-      this.field('content');
-      this.ref('id');
-    }
-  },
+    pages: {
+      indexCallback: function() {
+        this.field('title', {boost: 20});
+        this.field('description');
+        this.field('content');
+        this.ref('id');
+      }
+    },
 
-  staff: {
-    indexCallback: function() {
-      this.field('title', {boost: 20});
-      this.field('roles', {boost: 10});
-      this.field('bio');
-      this.ref('id');
+    staff: {
+      indexCallback: function() {
+        this.field('title', {boost: 20});
+        this.field('roles', {boost: 10});
+        this.field('bio');
+        this.ref('id');
+      }
     }
+  };
+
+  var searchAgent = new SearchAgent();
+  searchAgent.insert(searchModels);
+  for (var model in searchAgent.models) {
+    searchAgent.models[model].buildIndex();
+    searchAgent.models[model].getData();
+    searchAgent.models[model].fillIndex();
+    searchAgent.models[model].getTemplate();
   }
-};
-
-var searchAgent = ColMagSearchAgent();
-searchAgent.build();
+// })();
